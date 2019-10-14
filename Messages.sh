037a20e98ebeb73f2833b43e89dc3dd827259c5d
Yuri Chornoivan <yurchor@ukr.net>
2019-10-14 09:14:03 +0300

    Fix i18n

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/ikona.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/ikona.pot
