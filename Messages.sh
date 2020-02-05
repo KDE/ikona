0cade87eee2bb1df7d87566077cc15b384b32b16
Carson Black <uhhadd@gmail.com>
2020-02-05 16:26:37 -0500

    Update messages.sh to include ikonacli potfile

--- Messages.sh
+++ Messages.sh
@@ -3,3 +3,4 @@
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/ikona.pot
+$XGETTEXT `find src/rs/src/cli -name \*.rs` -o $podir/ikonacli.pot
